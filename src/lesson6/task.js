730654c8e76d9a683f225f1646b7d3677d97b594
wiksibot <wiksibot@gmail.com>
2018-02-15 13:16:47 +0200

    lesson6

--- src/lesson6/task.js
+++ src/lesson6/task.js
@@ -1,27 +1,53 @@
 // с помощью Fetch API и swapi.co API получить следующие данные
+import fetch from 'isomorphic-fetch';
+
+// function check status
+const status = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response);
+  }
+  return Promise.reject(new Error(response.statusText));
+};
 
 // Климат на любой планете по её имени
 // {planetName} – String
-const getClimate = function(planetName) {
-
+function getClimate(planetName) {
+  fetch(`https://swapi.co/api/planets/?search=${planetName}`)
+    .then(status)
+    .then(response => response.json())
+    .then(data => data.results[0].climate)
+    .catch(error => error);
 }
 
 // Получить информацию (Object) о любом персонаже по имени
 // {name} – String
-const getProfile = function(name) {
-
+function getProfile(name) {
+  fetch(`https://swapi.co/api/people/?search=${name}`)
+    .then(status)
+    .then(response => response.json())
+    .then(data => data.results[0])
+    .catch(error => error);
 }
 
 // Получить список пилотов (имена в виде Array of Strings) космического корабля
 // по его названию
 // {starshipName} - String
-const getPilots = function(starshipName) {
+const loadPilots = url => fetch(url)
+  .then(status)
+  .then(response => response.json());
 
+function getPilots(starshipName) {
+  fetch(`https://swapi.co/api/starships/?search=${starshipName}`)
+    .then(status)
+    .then(response => response.json())
+    .then(data => data.results[0].pilots)
+    .then(pilots => Promise.all(pilots.map(pilot => loadPilots(pilot))))
+    .then(data => data.map(pilot => pilot.name))
+    .catch(error => error);
 }
-
 
 export default {
   getClimate,
   getProfile,
-  getPilots
-}
+  getPilots,
+};
